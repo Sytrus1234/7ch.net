b82a2e972ec930037f369f62ac4cd37399e6b1af
Sytrus1234 <88072632+Sytrus1234@users.noreply.github.com>
2021-07-28 13:43:30 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>ようこそ</title>
+</head>
+
+<body>
+  <h1>ようこそ</h1>
+  <a href="log_in.php">ログイン</a>
+  <a href="new_member.php">新規登録</a>
+  <a href="">ゲストとしてログイン</a>
+</body>
+
+</html>
